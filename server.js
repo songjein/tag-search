e8f2c1faa49921b5d43bd1f51fb2f0333303a696
songjein <jeinsong100@gmail.com>
2018-11-20 13:14:46 +0000

    cors

--- server.js
+++ server.js
@@ -1,51 +1,55 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require('fs')
+const express = require('express')
 
-const server = express();
-const port = 5555;
+const server = express()
+const port = 5555
 
 const data = fs.readFileSync('mapping.json')
-const mapping = JSON.parse(data);
+const mapping = JSON.parse(data)
 
-console.log(mapping);
+server.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 server.get('/search', (req, res) => {
-	const tag = req.query.tag;
+	const tag = req.query.tag
 	candidate = []
 
 	for (let i = 0 ; i < tag.length; i ++){
-		candidate = candidate.concat(mapping[tag[i]]);
+		candidate = candidate.concat(mapping[tag[i]])
 	}
 
 	// Word Count
 	tmp = {}
 	for (let i = 0 ; i < candidate.length; i ++) {
 		if (candidate[i] in tmp) {
-			tmp[candidate[i]] += 1;
+			tmp[candidate[i]] += 1
 		} else {
-			tmp[candidate[i]] = 1;
+			tmp[candidate[i]] = 1
 		}
 	} 
 	
 	// Counting Sort
-	buffer = [];
-	const max_len = 50;
+	buffer = []
+	const max_len = 50
 	for (let i = 0 ; i < max_len ; i ++ ) {
-		buffer[i] = [];	
+		buffer[i] = []
 	}
 	for (let key in tmp){
-		buffer[tmp[key]].push(key);
+		buffer[tmp[key]].push(key)
 	}
 	
 	// Make sorted result
 	ret = []
 	for (let i = max_len - 1; i > 0 ; i --) {
-		ret = ret.concat(buffer[i]);
+		ret = ret.concat(buffer[i])
 	}
 
-	res.send(ret);
+	res.send(ret)
 });
 
 server.listen(port, () => {
-	console.log(`Server started at ${port}`);
+	console.log(`Server started at ${port}`)
 });
